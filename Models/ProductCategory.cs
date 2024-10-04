a570a015dc63ed5a65289fc3a9fed332afcf823e
joannriri <147371745+joannriri@users.noreply.github.com>
2024-10-04 23:09:46 +0800

    Add files via upload

---
+++ Models/ProductCategory.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TFL.Models
+{
+    public class ProductCategory
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
