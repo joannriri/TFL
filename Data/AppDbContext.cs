fb9babad35c4e5ca07502c32d0fd1f9c7faaa2db
joannriri <147371745+joannriri@users.noreply.github.com>
2024-10-04 23:12:40 +0800

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TFL.Models;
+namespace TFL.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<ProductCategory> Category { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<Orders> Orders { get; set; } 
+
+        
+    }
+}
