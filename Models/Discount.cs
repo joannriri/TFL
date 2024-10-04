32c78bbef8f549c271049936e92eeb309aeaeb30
joannriri <147371745+joannriri@users.noreply.github.com>
2024-10-04 23:07:36 +0800

    Add files via upload

---
+++ Models/Discount.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace TFL.Models
+{
+    public class Discount
+    {
+        [Key]
+        public int DiscountID { get; set; }
+        public string Name { get; set; }
+        [Precision(16, 2)]
+        public decimal DiscountPercent { get; set; }
+        
+        public Boolean Active { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
