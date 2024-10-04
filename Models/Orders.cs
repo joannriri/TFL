3486cc3cafe038defd169120fc7c7fa7d92f7348
joannriri <147371745+joannriri@users.noreply.github.com>
2024-10-04 23:08:48 +0800

    Add files via upload

---
+++ Models/Orders.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TFL.Models
+{
+    public class Orders
+    {
+        [Key]
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        [Required]
+        public int UserID { get; set; }
+        [ForeignKey("UserID")]
+        public virtual User User { get; set; }
+        [Precision(16,2)]
+        public decimal OrderTotal { get; set; }
+        [Required]
+        [ScaffoldColumn(false)]
+        public int OrderDetailID { get; set; }
+        [ForeignKey("OrderDetailID")]
+        public virtual OrderDetails OrderDetails { get; set; }
+    }
+}
