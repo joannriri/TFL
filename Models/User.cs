298157ea550c938ef6029d4d3f697ab39082a2d9
joannriri <147371745+joannriri@users.noreply.github.com>
2024-10-04 23:10:06 +0800

    Add files via upload

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TFL.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
