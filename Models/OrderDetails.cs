b22835d4ab3715736fe3b4ae22eeed35d6dcc175
joannriri <147371745+joannriri@users.noreply.github.com>
2024-10-04 23:08:12 +0800

    Add files via upload

---
+++ Models/OrderDetails.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TFL.Models
+{
+    public class OrderDetails
+    {
+        [Key]
+        public int OrderDetailID { get; set; }
+        [Required]
+        public int ProductID { get; set; }
+        [ForeignKey("ProductID")]
+        public virtual Product Product { get; set; }
+        public int Quantity { get; set; }
+        [Precision(16,2)]
+        public decimal UnitPrice { get; set; }
+    }
+}
