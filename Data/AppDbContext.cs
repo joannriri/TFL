ef9708bf248c53256d08b94793fa635c5f787b77
joannriri <147371745+joannriri@users.noreply.github.com>
2024-10-18 23:49:41 +0800

    updated AppDbContext

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,18 +1,43 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TFL.Areas.Identity.Data;
 using TFL.Models;
-namespace TFL.Data
-{
-    public class AppDbContext: DbContext
-    {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-        public DbSet<ProductCategory> Category { get; set; }
-        public DbSet<Discount> Discounts { get; set; }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Cart> Carts { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<OrderDetails> OrderDetails { get; set; }
-        public DbSet<Orders> Orders { get; set; } 
+using static NuGet.Packaging.PackagingConstants;
 
-        
+namespace TFL.Areas.Identity.Data;
+
+public class AppDbContext : IdentityDbContext<AppUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new AppUserEntityConfiguration());
+    }
+    public DbSet<ProductCategory> Category { get; set; }
+    public DbSet<Discount> Discounts { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<OrderDetails> OrderDetails { get; set; }
+    public DbSet<Orders> Orders { get; set; }
+
+}
+
+public class AppUserEntityConfiguration : IEntityTypeConfiguration<AppUser>
+{
+    public void Configure(EntityTypeBuilder<AppUser> builder)
+    {
+        builder.Property(u => u.FirstName).HasMaxLength(255);
+        builder.Property(u => u.LastName).HasMaxLength(255);
     }
 }
