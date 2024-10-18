1aff0300e6f6e836e582b04199c4b90f9d5c86c6
joannriri <147371745+joannriri@users.noreply.github.com>
2024-10-18 23:50:05 +0800

    UPDATED APP USER

---
+++ Data/AppUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace TFL.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the AppUser class
+public class AppUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public bool Gender { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
+
