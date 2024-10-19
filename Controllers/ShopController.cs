f61ae66a0422453af93f5459b7112e9adf586a3d
joannriri <147371745+joannriri@users.noreply.github.com>
2024-10-19 21:51:12 +0800

    updated shop controller

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -17,7 +17,7 @@ namespace TFL.Controllers
         {
             //retrieve products for seasonal genevieve 
             var products = _context.Products
-                .Where(p => p.CategoryID == 1004)
+                .Where(p => p.CategoryID == 1008)
                 .Include(p => p.ProductCategory)
                 .Include(p => p.Discount)
                 .AsQueryable();
@@ -70,7 +70,7 @@ namespace TFL.Controllers
         {
             //retrive products for Qalbi series
             var products = _context.Products
-                .Where(p => p.CategoryID == 1005)
+                .Where(p => p.CategoryID == 1009)
                 .Include(p => p.ProductCategory)
                 .Include(p => p.Discount)
                 .AsQueryable();
@@ -112,5 +112,99 @@ namespace TFL.Controllers
             return View("Views/Home/SeasonalQalbi.cshtml", products.ToList());
 
         }
+        public IActionResult Exclusives(string sortOrder, int? DiscountID, bool? inStock, bool? outOfStock)
+        {
+            //retrieve products for exclusives
+            var products = _context.Products
+                .Where(p => p.CategoryID == 1007)
+                .Include(p => p.ProductCategory)
+                .Include (p => p.Discount)
+                .AsQueryable();
+            
+            //apply filtering
+            if (DiscountID.HasValue)
+            {
+                products = products.Where(p => p.DiscountID == DiscountID.Value);
+            }
+            if (inStock.HasValue && inStock.Value)
+            {
+                products = products.Where(p => p.Stock > 0);
+            }
+            if (outOfStock.HasValue && outOfStock.Value)
+            {
+                products = products.Where(p => p.Stock == 0);
+            }
+
+            //apply sorting
+            switch (sortOrder)
+            {
+                case "name_asc":
+                    products = products.OrderBy(p => p.ProductName);
+                    break;
+                case "name_desc":
+                    products = products.OrderByDescending(p => p.ProductName);
+                    break;
+                case "price_asc":
+                    products = products.OrderBy(p => p.ProductPrice);
+                    break;
+                case "price_desc":
+                    products = products.OrderByDescending(p => p.ProductPrice);
+                    break;
+                default:
+                    products = products.OrderBy(p => p.ProductName); // Default sort
+                    break;
+            }
+            ViewBag.ProductCount = products.Count();
+            return View("Views/Home/Exclusives.cshtml", products.ToList());
+
+        }
+        public IActionResult Cotton(string sortOrder, int? DiscountID, bool? inStock, bool? outOfStock)
+        {
+            //retrieve products for exclusives
+            var products = _context.Products
+                .Where(p => p.CategoryID == 1006)
+                .Include(p => p.ProductCategory)
+                .Include(p => p.Discount)
+                .AsQueryable();
+
+            //apply filtering
+            if (DiscountID.HasValue)
+            {
+                products = products.Where(p => p.DiscountID == DiscountID.Value);
+            }
+            if (inStock.HasValue && inStock.Value)
+            {
+                products = products.Where(p => p.Stock > 0);
+            }
+            if (outOfStock.HasValue && outOfStock.Value)
+            {
+                products = products.Where(p => p.Stock == 0);
+            }
+
+            //apply sorting
+            switch (sortOrder)
+            {
+                case "name_asc":
+                    products = products.OrderBy(p => p.ProductName);
+                    break;
+                case "name_desc":
+                    products = products.OrderByDescending(p => p.ProductName);
+                    break;
+                case "price_asc":
+                    products = products.OrderBy(p => p.ProductPrice);
+                    break;
+                case "price_desc":
+                    products = products.OrderByDescending(p => p.ProductPrice);
+                    break;
+                default:
+                    products = products.OrderBy(p => p.ProductName); // Default sort
+                    break;
+            }
+            ViewBag.ProductCount = products.Count();
+            return View("Views/Home/Cotton.cshtml", products.ToList());
+
+        }
+
+    
     }
 }
