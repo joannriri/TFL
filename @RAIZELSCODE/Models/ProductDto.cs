067861fc5bf9eb3347b4524c7148a0875fae8ada
5651 <147370986+5L6C5B1@users.noreply.github.com>
2024-10-26 02:35:12 +0800

    Add ProductDto.cs
    to allow admin to save their edited CRUD changes and save them
    (do not migrate to db)

---
+++ @RAIZELSCODE/Models/ProductDto.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TheFabricsLab.Models
+{
+    public class ProductDto
+    {
+        [Required]
+        public string ProductName { get; set; }
+        [Required]
+        public int CategoryID { get; set; }
+        [Required]
+        public decimal ProductPrice { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public IFormFile? ImageFile { get; set; }
+        [Required]
+        public int Stock { get; set; }
+    }
+}
