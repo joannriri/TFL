1ce475e616ca864df296c7e8b9507cb1e1e8e00c
5651 <147370986+5L6C5B1@users.noreply.github.com>
2024-11-06 19:45:12 +0800

    Update Index.cshtml.cs
    added functionality to edit user account details

--- @RAIZELSCODE/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ @RAIZELSCODE/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -14,15 +14,18 @@ using TheFabricsLab.Areas.Identity.Data;
 
 namespace TheFabricsLab.Areas.Identity.Pages.Account.Manage
 {
-    [Authorize]
     public class IndexModel : PageModel
     {
         private readonly UserManager<AppUser> userManager;
-        public AppUser? appUser;
-        public IndexModel(UserManager<AppUser> userManager)
+        private readonly SignInManager<AppUser> signInManager;
+        
+        public IndexModel(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
             this.userManager = userManager;
+            this.signInManager = signInManager;
         }
+
+        public AppUser? appUser;
 
         [BindProperty]
         public InputModel Input { get; set; }
@@ -33,15 +36,67 @@ namespace TheFabricsLab.Areas.Identity.Pages.Account.Manage
             public string LastName { get; set; }
             public DateOnly DateOfBirth { get; set; }
             public string PhoneNumber { get; set; }
+            [Display(Name = "Subscribe to Newsletter?")]
             public bool NewsletterSubscription { get; set; }
         }
-        
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-            var task = userManager.GetUserAsync(User);
-            task.Wait();
-            appUser = task.Result;
+            var user = await userManager.GetUserAsync(User);
+            if (user != null)
+            {
+                appUser = user;
+                Input = new InputModel
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    DateOfBirth = user.DateOfBirth,
+                    PhoneNumber = user.PhoneNumber,
+                    NewsletterSubscription = user.NewsletterSubscription
+                };
+            }
         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = await userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
+            }
+
+            user.FirstName = Input.FirstName;
+            user.LastName = Input.LastName;
+            user.PhoneNumber = Input.PhoneNumber;
+            user.NewsletterSubscription = Input.NewsletterSubscription;
+
+            var result = await userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                await signInManager.RefreshSignInAsync(user);
+                TempData["SuccessMessage"] = "Your profile has been updated successfully!";
+                return RedirectToPage();
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+
+            return Page();
+        }
+
+
+        //public void OnGet()
+        //{
+        //    var task = userManager.GetUserAsync(User);
+        //    task.Wait();
+        //    appUser = task.Result;
+        //}
     }
 }
