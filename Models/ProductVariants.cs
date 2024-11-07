c95410a96e215279b4a544111c10ccc6f42d0a7f
joannriri <147371745+joannriri@users.noreply.github.com>
2024-11-07 11:25:08 +0800

    added ProductVariant

---
+++ Models/ProductVariants.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TFL.Models
+{
+    public class ProductVariants
+    {
+        [Key]
+        public int VariantID {  get; set; }
+        [Required]
+        public int ProductID { get; set; }
+        [ForeignKey("ProductID")]
+        public virtual Product Product { get; set; }
+        public string Color { get; set; }
+        [Precision(16, 2)]
+        public decimal Price { get; set; }
+        public int Stock {  get; set; }
+        public string ImageFile { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
