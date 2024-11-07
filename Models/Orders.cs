7ad8ec2b767fe66dee8d8c4ad47a3f3cf5b6f908
joannriri <147371745+joannriri@users.noreply.github.com>
2024-11-07 11:23:49 +0800

    Updated Orders

--- Models/Orders.cs
+++ Models/Orders.cs
@@ -10,11 +10,29 @@ namespace TFL.Models
         public int OrderID { get; set; }
         public DateTime OrderDate { get; set; }
         [Precision(16, 2)]
-        public decimal OrderTotal { get; set; }
-        [Required]
-        [ScaffoldColumn(false)]
-        public int OrderDetailID { get; set; }
-        [ForeignKey("OrderDetailID")]
-        public virtual OrderDetails OrderDetails { get; set; }
+        public decimal OrderTotal { get; set; }        
+        public string Email {  get; set; }
+        public string PhoneNumber { get; set; }
+
+        //Delivery Method
+        public string DeliveryMethod { get; set; }
+
+        //Shipping Details
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ShippingAddress { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        //Payment Method
+        public string PaymentMethod { get; set; }
+        public string BankSelect { get; set; }
+        
+        //Shipping Fee
+        [Precision(16,2)]
+        public decimal ShippingFee { get; set; }
+
+        public virtual ICollection<OrderDetails> OrderDetails { get; set; }
+
     }
 }
