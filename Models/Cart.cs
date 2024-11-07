5ab599513917dddaeb672a3888e3467ee1249511
joannriri <147371745+joannriri@users.noreply.github.com>
2024-11-07 11:23:15 +0800

    Updated Cart

--- Models/Cart.cs
+++ Models/Cart.cs
@@ -5,40 +5,36 @@ using System.ComponentModel.DataAnnotations;
 namespace TFL.Models
 {
     public class Cart
-    {
-        [Key]
-        public int CartID { get; set; }
-        [Required]
-        public int ProductID { get; set; }
-        [ForeignKey("ProductID")]
-        public virtual Product Products { get; set; }
+    {        
+            [Key]
+            public int CartID { get; set; }
+            [Required]
+            public int ProductID { get; set; }
+            [ForeignKey("ProductID")]
+            public virtual Product Products { get; set; }
 
-        public string ProductName { get; set; }
-        public int Quantity { get; set; }
-        [Precision(16, 2)]
-        public decimal Price { get; set; }
-        [Precision(16, 2)]
-        public decimal Total
-        {
-            get { return Quantity * Price; }
+            public string ProductName { get; set; }
+            public int Quantity { get; set; }
+            [Precision(16, 2)]
+            public decimal Price { get; set; }
+            [Precision(16, 2)]
+            public decimal Total
+            {
+                get { return Quantity * Price; }
 
-        }
-        public string Image { get; set; }
-        public Cart()
-        {
+            }
+            public string Image { get; set; }
+            [Required]
+            [ScaffoldColumn(false)]
+            public int VariantID { get; set; }
+            [ForeignKey("VariantID")]
+            public virtual ProductVariants Variants { get; set; }
+            
+         
 
-        }
-        public Cart(Product product)
-        {
-            ProductID = product.ProductID;
-            ProductName = product.ProductName;
-            Price = product.ProductPrice;
-            Quantity = 1;
-            Image = product.ImageFile;
-
-        }
+            
 
 
-
+        
     }
 }
